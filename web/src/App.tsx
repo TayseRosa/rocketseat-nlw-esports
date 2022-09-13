ea276b4436e10a9975b6388e6eb89364053e563f
Tayse Rosa <tayse_rosa@yahoo.com.br>
2022-09-12 23:12:42 -0300

    FInalizando

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -79,7 +79,7 @@ function App() {
           <span className="text-zinc-400 block" >Publique um anúncio para encontrar novos players!</span>
         </div>
 
-      <button className="py-3 px-4 bg-violet-500 hover: bg-violet-600 text-white rounded flex items-center gap-3">
+      <button className="py-3 px-4 bg-violet-500 hover: bg-violet-600 text-white rounded flex items-center">
         <MagnifyingGlassPlus size={24} />
         Publicar anúncio
       </button>
